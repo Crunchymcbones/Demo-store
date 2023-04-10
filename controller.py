de62c9165bee86e1cee8b67d224f3098a4f3e486
Jasta1234 <Crockerjoshua782@gmail.com>
2023-04-10 12:23:40 -0230

    functions to add and edit customers, invoices

--- controller.py
+++ controller.py
@@ -91,3 +91,23 @@ def getProductQtyByName(product_name):
     sql = f"select product_id, in_store_qty from `easy_cheese`.`products` where name = '{product_name}';"
     return executeQueryAndReturnResult(sql)
 
+def addCustomer(fname,lname, address, email, phone):
+    sql = f"INSERT INTO `easy_cheese`.`customers` (`first_name`, `last_name`, `address`, `email_address`, `phone_number`) VALUES ('{fname}', '{lname}', '{address}', '{email}', '{phone}');"
+    return executeQueryAndCommit(sql)
+
+def editCustomer(cid, fname,lname, address, email, phone):
+    sql = f"UPDATE `easy_cheese`.`customers` SET `first_name` = '{fname}', `last_name` = '{lname}', `address` = '{address}', `email_address` = '{email}', `phone_number` = '{phone}' WHERE (`customer_id` = '{cid}');"
+    return executeQueryAndCommit(sql)
+
+def getInvoices():
+    sql = f"select * from `easy_cheese`.`invoices`;"
+    return executeQueryAndReturnResult(sql)
+
+def getActiveCustomers():
+    sql = f"SELECT customer_id, date FROM easy_cheese.invoices where date >= date_sub(now(), Interval 1 month);"
+    return executeQueryAndReturnResult(sql)
+
+def getOutOfStock():
+    sql = f"SELECT * FROM easy_cheese.products where in_store_qty = 0;"
+    return executeQueryAndReturnResult(sql)
+
--- main.py
+++ main.py
@@ -15,6 +15,11 @@ class MainWindow(QMainWindow):
         self.initializeCheckoutWindow()
         self.initializeInventory()
         self.initializeAllProductsTable()
+        self.initializeCustomers()
+        self.initializeManager()
+
+        self.initializeInvoicesTable()
+        self.initializeActiveCustomersTable()
 
     def initializeCheckoutWindow(self):
         self.coProdNameCbo = self.findChild(QComboBox, 'coProdNameCbo')
@@ -257,6 +262,129 @@ class MainWindow(QMainWindow):
         self.invTbl = self.findChild(QTableWidget, 'invTbl')
         colNames, data = getAllProducts()
         self.displayDataInTable(colNames, data, self.invTbl)
+
+    def initializeCustomers(self):
+        self.acfnameLineEdit = self.findChild(QLineEdit, 'acfnameLineEdit')
+        self.aclnameLineEdit = self.findChild(QLineEdit, 'aclnameLineEdit')
+        self.acaddressLineEdit = self.findChild(QLineEdit, 'acaddressLineEdit')
+        self.acemailLineEdit = self.findChild(QLineEdit, 'acemailLineEdit')
+        self.acphoneLineEdit = self.findChild(QLineEdit, 'acphoneLineEdit')
+        self.acBtn = self.findChild(QPushButton, 'acBtn')
+        self.acBtn.clicked.connect(self.acBtnClickHandler)
+        self.acLabel = self.findChild(QLabel, 'acLabel')
+
+        self.eccidLineEdit = self.findChild(QLineEdit, 'eccidLineEdit')
+        self.ecfnameLineEdit = self.findChild(QLineEdit, 'ecfnameLineEdit')
+        self.eclnameLineEdit = self.findChild(QLineEdit, 'eclnameLineEdit')
+        self.ecaddressLineEdit = self.findChild(QLineEdit, 'ecaddressLineEdit')
+        self.ecemailLineEdit = self.findChild(QLineEdit, 'ecemailLineEdit')
+        self.ecphoneLineEdit = self.findChild(QLineEdit, 'ecphoneLineEdit')
+        self.ecBtn = self.findChild(QPushButton, 'ecBtn')
+        self.ecBtn.clicked.connect(self.ecBtnClickHandler)
+        self.ecLabel = self.findChild(QLabel, 'ecLabel')
+
+    def initializeManager(self):
+        self.initializeInvoicesTable()
+        self.initializeActiveCustomersTable()
+        self.initializeOutOfStockTable()
+
+    def acBtnClickHandler(self):
+        try:
+            fname = self.acfnameLineEdit.text()
+            lname = self.aclnameLineEdit.text()
+            address = self.acaddressLineEdit.text()
+            email = self.acemailLineEdit.text()
+            phone = self.acphoneLineEdit.text()
+            result = addCustomer(fname, lname, address, email, phone)
+        except Exception as e:
+            self.acLabel.setText(str(e))
+        else:
+            if result == 1:
+                self.acLabel.setText('Customer added')
+            else:
+                self.acLabel.setText('Customer not added')
+
+        self.refreshCustomersTab()
+
+    def ecBtnClickHandler(self):
+        try:
+            cid = self.eccidLineEdit.text()
+            fname = self.ecfnameLineEdit.text()
+            lname = self.eclnameLineEdit.text()
+            address = self.ecaddressLineEdit.text()
+            email = self.ecemailLineEdit.text()
+            phone = self.ecphoneLineEdit.text()
+            result = editCustomer(cid, fname, lname, address, email, phone)
+        except Exception as e:
+            self.ecLabel.setText(str(e))
+        else:
+            if result == 1:
+                self.ecLabel.setText('Customer updated')
+            else:
+                self.ecLabel.setText('Customer not updated')
+        self.refreshCustomersTab()
+
+    def refreshCustomersTab(self):
+        self.acfnameLineEdit.clear()
+        self.aclnameLineEdit.clear()
+        self.acaddressLineEdit.clear()
+        self.acemailLineEdit.clear()
+        self.acphoneLineEdit.clear()
+
+        self.eccidLineEdit.clear()
+        self.ecfnameLineEdit.clear()
+        self.eclnameLineEdit.clear()
+        self.ecaddressLineEdit.clear()
+        self.ecemailLineEdit.clear()
+        self.ecphoneLineEdit.clear()
+
+    def initializeInvoicesTable(self):
+        self.invoicesTableWidget = self.findChild(QTableWidget, 'invoicesTableWidget')
+        colNames, data = getInvoices()
+        self.displayInvoiceDataInTable(colNames, data, self.invoicesTableWidget)
+
+    def initializeActiveCustomersTable(self):
+        self.activeTableWidget = self.findChild(QTableWidget, 'activeTableWidget')
+        colNames, data = getActiveCustomers()
+        self.displayInvoiceDataInTable(colNames, data, self.activeTableWidget)
+
+    def initializeOutOfStockTable(self):
+        self.outOfStockTable = self.findChild(QTableWidget, 'outOfStockTable')
+        colNames, data = getOutOfStock()
+        self.displayStockDataInTable(colNames, data, self.outOfStockTable)
+
+    def displayInvoiceDataInTable(self, columns, rows, table: QTableWidget):
+        table.setRowCount(len(rows))
+        table.setColumnCount(len(columns))
+        for i in range(len(rows)):
+            row = rows[i]
+            for j in range(len(row)):
+                table.setItem(i, j, QTableWidgetItem(str(row[j])))
+        columns = ['Invoice_ID', 'Customer_ID', 'Invoice_Total', 'Date']
+        for i in range(table.columnCount()):
+            table.setHorizontalHeaderItem(i, QTableWidgetItem(f'{columns[i]}'))
+
+    def displayActiveCustomersInTable(self, columns, rows, table: QTableWidget):
+        table.setRowCount(len(rows))
+        table.setColumnCount(len(columns))
+        for i in range(len(rows)):
+            row = rows[i]
+            for j in range(len(row)):
+                table.setItem(i, j, QTableWidgetItem(str(row[j])))
+        columns = ['Customer_ID', 'Last_Purchase']
+        for i in range(table.columnCount()):
+            table.setHorizontalHeaderItem(i, QTableWidgetItem(f'{columns[i]}'))
+
+    def displayStockDataInTable(self, columns, rows, table: QTableWidget):
+        table.setRowCount(len(rows))
+        table.setColumnCount(len(columns))
+        for i in range(len(rows)):
+            row = rows[i]
+            for j in range(len(row)):
+                table.setItem(i, j, QTableWidgetItem(str(row[j])))
+        columns = ['Product_ID', 'Product_Name', 'Description', 'Vendor_ID', 'Quantity', 'Price']
+        for i in range(table.columnCount()):
+            table.setHorizontalHeaderItem(i, QTableWidgetItem(f'{columns[i]}'))
 
 
 if __name__ == '__main__':
